2ffeadf483223ec3966e86236e4e30048bfde53c
petr-skokan <petr.skokan@student.sps-cl.cz>
2023-09-18 15:10:22 +0200

    skoro hotovy

---
+++ LibraryApp/LibraryApp/moduls/DVD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.moduls
+{
+    internal class DVD
+    {
+        internal class DVD : libraryitem<DVD>
+        {
+            public int Capacity { get; }
+            public string Author { get; }
+
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Autor {Author} a {Capacity} velikost (MB) ");
+        }
+    }
+}
---
+++ LibraryApp/LibraryApp/moduls/libraryitem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.moduls
+{
+    internal abstract class libraryitem
+    {
+        private int Id { get; }
+        private string Title { get; }
+        public bool IsAvailable { get; set; }
+
+        public libraryitem(int Id, string Title)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.IsAvailable = true;
+        }
+
+        public abstract void DisplayInfo();
+
+    }
+}
